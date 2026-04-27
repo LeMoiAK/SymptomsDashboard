42b794174c4d8858b11d6656ad6932fe2d1718f8
LeMoiAK <136517039+LeMoiAK@users.noreply.github.com>
2026-04-27 19:16:49 +0100

    Using script to init db with app_user

---
+++ db/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER app_user WITH PASSWORD '${APP_DB_PASSWORD}';
+
+    GRANT CONNECT ON DATABASE app_db TO app_user;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname app_db <<-EOSQL
+    GRANT USAGE ON SCHEMA public TO app_user;
+
+    GRANT SELECT, INSERT, UPDATE, DELETE
+    ON ALL TABLES IN SCHEMA public
+    TO app_user;
+
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
+EOSQL
