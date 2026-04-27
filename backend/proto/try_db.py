e8f06780d378c61953e5809d52b060fbb7de81c3
LeMoiAK <136517039+LeMoiAK@users.noreply.github.com>
2026-04-27 23:27:07 +0100

    use sqlalchemy instead of psycopg2 directly

--- backend/proto/try_db.py
+++ backend/proto/try_db.py
@@ -9,22 +9,16 @@ from pydantic_settings import BaseSettings
 class DbConfig(BaseSettings):
     APP_DB_USER: str = Field(min_length=1)
     APP_DB_PASSWORD: str = Field(min_length=1)
+    DATABASE_URL: str = Field(min_length=1)
 
 config = DbConfig()
 
 # Then connect to DB
 print(f"User: {config.APP_DB_USER} | pass: {config.APP_DB_PASSWORD}")
-import psycopg2
-conn = psycopg2.connect(database = "app_db", 
-                        user = config.APP_DB_USER, 
-                        host = 'localhost',
-                        password = config.APP_DB_PASSWORD,
-                        port = 5432)
 
-cur = conn.cursor()
-cur.execute('SELECT * FROM patients;')
-rows = cur.fetchall()
-conn.commit()
+import sqlalchemy as db
+engine = db.create_engine(f"postgresql://{config.APP_DB_USER}:{config.APP_DB_PASSWORD}@localhost:5432/app_db")
+conn = engine.connect() 
+output = conn.execute("SELECT * FROM public.patients")
+print(output.fetchall())
 conn.close()
-for row in rows:
-    print(row)
